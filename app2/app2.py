8826601640e4b7de7622ddd0b2cc53050218b5c6
raisalpa <raisal.pa@fullcontact.com>
2022-06-11 16:46:54 +0530

    initial

---
+++ app2/app2.py
@@ -0,0 +1,14 @@
+# import the Flask class from the flask module
+from flask import Flask
+
+# create the application object
+app = Flask(__name__)
+
+# use decorators to link the function to a url
+@app.route('/')
+def home():
+    return "This is App2"  # return a string
+
+# start the server with the 'run()' method
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
